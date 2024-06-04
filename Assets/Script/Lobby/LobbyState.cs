0d272e4e94be3ad69fb516369ba9a1083413515a
SekineSeishu <mika242401@gmail.com>
2024-06-05 01:48:43 +0900

    修正

--- Assets/Script/Lobby/LobbyState.cs
+++ Assets/Script/Lobby/LobbyState.cs
@@ -7,14 +7,15 @@ public class LobbyState : NetworkBehaviour
 {
     [Networked] public int JoinPlayerCount { get; private set; } = 0;
 
-    /*public override void Spawned()
+    public override void Spawned()
     {
         if (Runner.IsServer)
         {
-            JoinPlayerCount = 0; // ロビー作成者をカウント
+            JoinPlayerCount = 0; 
         }
-    }*/
+    }
 
+    //プレイヤーの数を増やす
     public void IncrementJoinPlayerCount()
     {
         if (Runner.IsServer)
