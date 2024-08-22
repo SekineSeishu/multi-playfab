60a47ab0f350045ee3faf3e14b2301ebf310cc77
sekineseishu <mika242401@gmail.com>
2024-08-22 15:05:02 +0900

    ランダムな文字列を形成する

--- Assets/Script/Lobby/CodeGenerator.cs
+++ Assets/Script/Lobby/CodeGenerator.cs
@@ -7,10 +7,12 @@ using System.Runtime.InteropServices.WindowsRuntime;
 public static class CodeGenerator
 {
     private static readonly Random random = new Random();
+    //選ばれる英数字
     private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     public static string GenerateCode(int length)
     {
+        //指定した桁数文Charsからランダムに選ぶ
         return new string(Enumerable.Repeat(Chars, length)
            .Select(s => s[random.Next(s.Length)]).ToArray());
     }
