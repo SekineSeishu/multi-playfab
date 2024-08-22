ee3b63ff0aa5518b92c79a5832354e911e4bf679
sekineseishu <mika242401@gmail.com>
2024-08-22 15:05:33 +0900

    ロビー内からのシーン移動管理

--- Assets/Script/Lobby/LobbySceneController.cs
+++ Assets/Script/Lobby/LobbySceneController.cs
@@ -6,13 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class LobbySceneController : MonoBehaviour
 {
-    [SerializeField] private LobbyUIManager lobbyUIManager;
-    private GameObject playerPrefab;
-    private NetworkRunner runner;
-    private string lobbyName;
-    [SerializeField] private string homeSceneName = "";
-    [SerializeField] private GameObject backHomeUI;
-    [SerializeField] private LobbyState lobbyState;
+    [SerializeField] private LobbyUIManager lobbyUIManager;//ロビーUIの管理
+    private GameObject playerPrefab;//プレイヤーオブジェクト
+    private NetworkRunner runner;//ネットワークランナー
+    private string lobbyName;//ロビー名
+    [SerializeField] private string homeSceneName = "";//メニューシーン名入力
+    [SerializeField] private GameObject backHomeUI;//メニュー画面に戻る
+    [SerializeField] private LobbyState lobbyState;//ロビー情報
     public bool spaw;
     // Start is called before the first frame update
     void Start()
@@ -23,39 +23,26 @@ public class LobbySceneController : MonoBehaviour
             lobbyName = GameManager.Instance.CurrentLobbyName;
             runner = LobbyManager.Instance._runner;
             OnPlayerJoined(runner);
-            //lobbyUIManager.SetLobby(lobbyName, playerPrefab, runner);
         }
     }
     public void OnPlayerJoined(NetworkRunner runner)
     {
-        if (spaw)
-        {
-            //Debug.LogError("aaa");
-            //PlayerSpawner.Instance.PlayerJoined(runner);
-        }
-        // プレイヤー数を更新
-        //lobbyUIManager.UpdatePlayerCount(runner.SessionInfo.PlayerCount);
     }
+    //退出画面を出す
     public void BackHomeMenu()
     {
         backHomeUI.SetActive(true);
     }
 
+    //退出画面を消す
     public void BackLobby()
     {
         backHomeUI.SetActive(false);
     }
-
+    //メニューシーンに戻る
     public void BackHome()
     {
         SceneManager.LoadScene(homeSceneName);
-        /*Player SelectPlayer = lobbyUIManager.playerList.Find(Player => Player._name == player._name);
-        if (SelectPlayer == null)
-        {
-            Debug.Log("not player");
-        }
-        //lobbyUIManager.playerList.Remove(SelectPlayer);
-        lobbyUIManager.UpdateLobby();*/
     }
 
     // Update is called once per frame
