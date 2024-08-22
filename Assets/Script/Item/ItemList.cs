3bc5371a860fbdfcda0910325036f40e68c67266
sekineseishu <mika242401@gmail.com>
2024-08-22 15:04:35 +0900

    アイテムのリスト管理

--- Assets/Script/Item/ItemList.cs
+++ Assets/Script/Item/ItemList.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class ItemList : MonoBehaviour
 {
     public static ItemList Instance;
-    public List<Item> allItems;
-    public List<Item> inventoryItems;
-    public List<Item> shopItems;
+    public List<Item> allItems;//ゲームにある全てのアイテムデータリスト
+    public List<Item> inventoryItems;//インベントリにあるアイテムデータリスト
+    public List<Item> shopItems;//ショップにあるアイテムデータリスト
     private void Awake()
     {
         Instance = this;
